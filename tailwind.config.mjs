8e635542646a41e3f398b76848096c512934d17e
Pablo <guinazuzu@gmail.com>
2024-03-24 04:49:25 -0300

    Se añadio 2 nuevos componentes un fondo con animacion de degradado de colores y una flecha con animacion, para la animacion se modifico tailwind.config añadiendo variables de css : backgroundsize, animation y kayframes, por ultimo se le dio un poco mas de espacio al nav en el header y se añadieron los componentes al index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundSize:{
+				'200%*2':'200% 200%',
+			},
+			animation: {
+				Movecolor: 'MoveColor 5s linear infinite alternate',
+			},
+			keyframes: {
+				MoveColor:{
+					"0%":{ "background-position": "0 0"},
+					"100%" :{ "background-position": "100% 100%"},
+				},
+			},
+
+		},
 	},
 	plugins: [],
 }
