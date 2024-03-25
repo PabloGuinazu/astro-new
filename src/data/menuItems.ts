2b40c7457ac4800c12754e79be284f2cdc701da0
Pablo <guinazuzu@gmail.com>
2024-03-25 00:50:27 -0300

    Se cambio el header con el nav se modifico el codigo para usar cporrectamente el layout, se usa ts para el menu y css para que el menu sea mas agradable

---
+++ src/data/menuItems.ts
@@ -0,0 +1,18 @@
+import type { menuItem } from "../types/types";
+
+const menuItems:menuItem[] =[
+    {
+        title:'Inicio',
+        url:'/'
+    },
+    {
+        title:'Nosotros',
+        url:'/Nosotros'
+    },
+    {
+        title:'Contactanos',
+        url:'/Contactanos'
+    },
+]
+
+export default menuItems;
